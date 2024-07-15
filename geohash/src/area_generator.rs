e6a62026fda2c3c8038c6b086b17e0cf0ea32db4
reverendlovejoy43782 <alias23423@mailbox.org>
2024-07-15 13:41:37 +0200

    maps above input fields

--- geohash/src/area_generator.rs
+++ geohash/src/area_generator.rs
@@ -3,6 +3,8 @@
 
 use ic_cdk::export::candid::{CandidType, Deserialize};
 
+// Constants for converting between meters and degrees
+const METERS_IN_DEGREE: f64 = 111320.0; // Approximate, varies by latitude
 
 #[derive(Debug, CandidType, Deserialize)]
 pub struct Area {
@@ -11,6 +13,33 @@ pub struct Area {
     pub lat_end: f64,
     pub lon_end: f64,
 }
+
+pub fn calculate_area(lat: f64, lon: f64) -> Area {
+    // 500 meters in degrees
+    let half_side_length = 500.0 / METERS_IN_DEGREE;
+
+    Area {
+        lat_start: lat - half_side_length,
+        lon_start: lon - half_side_length,
+        lat_end: lat + half_side_length,
+        lon_end: lon + half_side_length,
+    }
+}
+
+
+/*
+
+use ic_cdk::export::candid::{CandidType, Deserialize};
+
+
+#[derive(Debug, CandidType, Deserialize)]
+pub struct Area {
+    pub lat_start: f64,
+    pub lon_start: f64,
+    pub lat_end: f64,
+    pub lon_end: f64,
+}
+
 
 
 pub fn calculate_area(lat: f64, lon: f64) -> Area {
@@ -30,6 +59,7 @@ pub fn calculate_area(lat: f64, lon: f64) -> Area {
         lon_end: upper_lon,
     }
 }
+*/
 
 
 
