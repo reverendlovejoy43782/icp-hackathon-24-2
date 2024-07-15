8eca30edb311ea163f631ecd3f29f387a1da57c1
reverendlovejoy43782 <alias23423@mailbox.org>
2024-07-15 09:01:19 +0200

    added Cargo.toml in root

---
+++ .dfx/local/canisters/geohash/service.did.d.ts
@@ -0,0 +1,29 @@
+import type { Principal } from '@dfinity/principal';
+import type { ActorMethod } from '@dfinity/agent';
+import type { IDL } from '@dfinity/candid';
+
+export interface AreaResponse {
+  'lat_start' : number,
+  'lon_start' : number,
+  'geohash' : string,
+  'lat_end' : number,
+  'lon_end' : number,
+}
+export interface Geolocation { 'latitude' : number, 'longitude' : number }
+export interface _SERVICE {
+  'query_compute_area' : ActorMethod<[string], AreaResponse>,
+  'query_compute_geohash' : ActorMethod<
+    [Geolocation],
+    [
+      {
+        'lat_start' : number,
+        'lon_start' : number,
+        'lat_end' : number,
+        'lon_end' : number,
+      },
+      string,
+    ]
+  >,
+}
+export declare const idlFactory: IDL.InterfaceFactory;
+export declare const init: (args: { IDL: typeof IDL }) => IDL.Type[];
